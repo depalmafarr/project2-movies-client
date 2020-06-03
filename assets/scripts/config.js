7b9538debcb6c09ba046ebecf66de4e3be2ef525
depalmafarr <brandondepalmafarr@gmail.com>
2020-06-03 14:11:39 -0400

    Fixed config bug
    Took out / form production url name in config.js

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://movie-logs-09.herokuapp.com/',
+  production: 'https://movie-logs-09.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
